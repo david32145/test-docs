43044af68097bd3259e001028a22942847939ea1
rafaelvieirab <rafaelviebar@gmail.com>
2020-10-05 10:40:49 -0300

    chore/dashboard_with_vue: adicionando axios.

--- web2/src/main.js
+++ web2/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
